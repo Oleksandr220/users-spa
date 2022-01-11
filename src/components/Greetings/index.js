1318ea8a7690843e918adc675b2eb9433b16c0e2
user <oleksnadrmaslov663@gmail.com>
2022-01-11 13:58:24 +0200

    translate greetings

--- src/components/Greetings/index.js
+++ src/components/Greetings/index.js
@@ -1,3 +1,14 @@
+import '../../services/i18n';
+import { Trans, useTranslation } from 'react-i18next';
+
 export default function Greetings() {
-  return <h1 className={'home-title'}>Welcome to single page application.</h1>;
+  const { i18n } = useTranslation();
+
+  return (
+    <h1 className={'home-title'}>
+      <Trans i18nKey="description.part7">
+        Welcome to single page application
+      </Trans>
+    </h1>
+  );
 }
